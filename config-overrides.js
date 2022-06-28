50525311a4d6cae35475a6ac3b984712767e1d5b
agencik007 <agencik007@gmail.com>
2022-06-28 21:29:24 +0200

    Initial commit.

---
+++ config-overrides.js
@@ -0,0 +1,8 @@
+const {override} = require('customize-cra');
+const {aliasWebpack, configPaths} = require('react-app-alias-ex');
+
+module.exports = {
+  webpack: override(
+    aliasWebpack(configPaths('./tsconfig.paths.json'))
+  ),
+}
